7a80f478c6f77a6e743319e2fa13d48a5e394609
tonghao.yuan <tonghao.yuan@csdental.com>
2021-07-19 15:20:50 +0800

    feat: high dpi

---
+++ src/qml_dpi/main.cpp
@@ -0,0 +1,22 @@
+#include "qcoreapplication.h"
+#include "qnamespace.h"
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+auto main(int argc, char* argv[]) -> int {
+  QGuiApplication app(argc, argv);
+  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+  QQmlApplicationEngine engine;
+  const QUrl url(QStringLiteral("qrc:/main.qml"));
+  QObject::connect(
+    &engine, &QQmlApplicationEngine::objectCreated, &app,
+    [url](QObject* obj, const QUrl& objUrl) {
+      if (!obj && url == objUrl)
+        QCoreApplication::exit(-1);
+    },
+    Qt::QueuedConnection);
+  engine.load(url);
+
+  return QGuiApplication::exec();
+}
---
+++ src/qml_dpi/main.qml
@@ -0,0 +1,14 @@
+import QtQuick 2.12
+import QtQuick.Controls 2.12
+import QtQuick.Window 2.12
+
+Window {
+    visible: true
+    width: 800
+    height: 600
+    Text {
+        anchors.centerIn: parent
+        text: "Center"
+        font.pointSize: 80
+    }
+}
